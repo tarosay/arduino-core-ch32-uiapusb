69fe809f7c01d8495526a5f792a9d87358e6a112
Minao Tarosay Yamamoto <tarosa.y@gmail.com>
2026-04-11 06:23:41 +0900

    v0.0.4

--- cores/arduino/main.cpp
+++ cores/arduino/main.cpp
@@ -3,11 +3,14 @@
 #include "Arduino.h"
 #include "uiapusb.h"
 
+extern "C" void pre_init(void);
+
 extern "C" int main(void)
 {
     uiapusb_begin();
     setup();
-    while (1) {
+    while (1)
+    {
         loop();
     }
 }
